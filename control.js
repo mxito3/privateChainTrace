4cdbe3c66368a8281f8001a6de50207c6b801cc4
mxito3 <1477311729@qq.com>
2018-08-14 13:39:21 +0800

    add control.js

--- control.js
+++ control.js
@@ -1,24 +1,66 @@
 /*
  * @Author: YP
- * @Date:   2018-08-13 22:33:33
+ * @Date:   2018-08-14 10:18:20
  * @Last Modified by:   YP
- * @Last Modified time: 2018-08-13 23:57:06
+ * @Last Modified time: 2018-08-14 13:32:29
  */
 Web3 = require('web3');
 
 var App = {
-     mint:function(peerIndex) {
-        var indexStart = 9998;
-        w3 = new Web3(new Web3.providers.HttpProvider("http://localhost:" + (indexStart + peerIndex)));
-        if(w3.isConnected())
+    web3: [],
+    init: function(peerIndex) {
+        var indexStart = 8545;
+        for (let i = 1; i <= 5; i++) {
+            App.web3[i] = new Web3(new Web3.providers.HttpProvider("http://localhost:" + (i + indexStart)));
+            if (App.web3[i].isConnected()) {
+                console.log("连接第"+i+"个节点成功");
+                // -console.log(w3);
+                // w3.miner.start();
+            } else {
+                console.log("连接失败");
+            }
+            // indexStart++;
+        }
+
+
+    },
+    unlock: function() {
+    
+        for (let i = 1; i <= 5; i++)
         {
-        	console.log("连接成功");
-        	console.log(w3);
+            // console.log(App.web3[i].eth.coinbase);
+            if(i==1)
+            {
+                App.web3[i].personal.unlockAccount(App.web3[i].eth.coinbase,"domore0325",100000000,App.callback);
+
+            }
+            else
+            {
+                App.web3[i].personal.unlockAccount(App.web3[i].eth.coinbase,"123456",100000000,App.callback);
+            }
+        }
+    },
+    callback: function(error,data)
+    {
+        if(!error)
+        {
+            console.log(data);
         }
         else
         {
-        	console.log("连接失败");
+            console.log(error);
         }
+    },
+    getCoinbase:function()
+    {
+        for (let i = 1; i <= 5; i++)
+        {
+            console.log(i+"         "+App.web3[i].eth.coinbase) ;   
+        }
+        
     }
+
 };
-App.mint(1);
+App.init();
+App.unlock();
+App.getCoinbase();
